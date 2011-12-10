2567c3b7255c444885326de83eeec39a09f8a909
Jason K <precursor@gmail.com>
2011-12-10 12:37:39 +0200

    Fixed bench_series

--- RayTetra/bench_series.sh
+++ RayTetra/bench_series.sh
@@ -5,13 +5,13 @@ make clean cleanbak cleanoutput
 make all
 
 #Main Benchmark
-./full_bench 1512000 10 10
+./fullbench.sh 1512000 10 10
 
 #Caching Benchmarks
-./full_bench 1000000 1 10
-./full_bench 100000 10 10
-./full_bench 10000 100 10
-./full_bench 1000 1000 10
-./full_bench 100 10000 10
-./full_bench 10 100000 10
-./full_bench 1 1000000 10
+./fullbench.sh 1000000 1 10
+./fullbench.sh 100000 10 10
+./fullbench.sh 10000 100 10
+./fullbench.sh 1000 1000 10
+./fullbench.sh 100 10000 10
+./fullbench.sh 10 100000 10
+./fullbench.sh 1 1000000 10
